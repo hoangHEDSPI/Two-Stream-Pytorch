860807eeaeb43f539acba8c5f1c02acb1e818dd4
hoanghedspi <a2k40pbc@gmail.com>
2018-10-17 18:28:15 +0700

    done : Network.py

--- models/network.py
+++ models/network.py
@@ -76,7 +76,31 @@ class Bottleneck(nn.Module):
 
 class ResNet(nn.Module):
 
-    def __init__(self):
+    def __init__(self, block, layers, nb_classes=101, channel=20):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+        self.conv1_custom = nn.Conv2d(channel, 64, kernel_size=7, stride=2,
+                                      padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+
+        self.avgpool = nn.AvgPool2d(7)
+        self.fc_custom = nn.Linear(512*block.expansion, nb_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                m = m.kernel_size[0]*m.kernel_size[1]*m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2./n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
 
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
@@ -94,5 +118,54 @@ class ResNet(nn.Module):
             layers.append(block(self.in_planes, planes))
 
         return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1_custom(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        out = self.fc_custom(x)
+        return out
+
+def resnet18(pretrained=False, channel=20, **kwargs):
+
+    model = ResNet(BasicBlock, [2,2,2,2], nb_classes=101, channel=channel)
+    if pretrained:
+        pretrain_dict = model_zoo.load_url(model_url)
+        model_dict = model.state_dict()
+        model_dict = weight_transform(model_dict, pretrain_dict, channel)
+        model.load_state_dict(model_dict)
+    return model
+
+
+def cross_modality_pretrain(conv1_weight, channel):
+    S = 0
+    for i in range(3):
+        S += conv1_weight[:, i, :, :]
+    avg = S/3
+    new_conv1_weight = torch.FloatTensor(64, channel, 7, 7)
+    for i in range(channel):
+        new_conv1_weight[:,i,:,:] = avg.data
+    return new_conv1_weight
+
+def weight_transform(model_dict, pretrain_dict, channel):
+    weight_dict = {k:v for k, v in pretrain_dict.items() if k in model_dict}
+    w3 = pretrain_dict['conv1.weight']
+    if channel == 3:
+        wt = w3
+    else:
+        wt = cross_modality_pretrain(w3, channel)
+
+    weight_dict['conv1_custom.weight'] = wt
+    model_dict.update(weight_dict)
+    return model_dict
 
 
