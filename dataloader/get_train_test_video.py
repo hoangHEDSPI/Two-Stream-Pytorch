134a186b47a0304aee67ed88d60e6ccca516a597
hoanghedspi <a2k40pbc@gmail.com>
2018-10-21 15:18:29 +0700

    added utils.py

--- dataloader/get_train_test_video.py
+++ dataloader/get_train_test_video.py
@@ -2,8 +2,9 @@ import os, pickle
 
 class UCF101_splitter():
 
-    def __init__(self, path):
+    def __init__(self, path, split):
         self.path = path
+        self.split = split
 
     def get_action_index(self):
         self.action_label = {}
@@ -14,5 +15,19 @@ class UCF101_splitter():
             index, label = line.split(" ")
             if index not in self.action_label.keys():
                 self.action_label[index] =  label
+
+    def file_2_dic(self, filename):
+        with open(filename, "r") as f:
+            content = f.readlines()
+            content = [x.strip("\r\n") for x in content]
+        dic = {}
+        for line in content:
+            video_name = line.split("/", 1)[1].split(" ", 1)[0]
+            key = video_name.split("_",1)[1].split(".",1)[0]
+            label = self.action_label[line.split("/")[0]]
+            dic[key] = int(label)
+        return dic
+
+
 
 
